a93bc2dbe1fece3ca2a348364e93997c576c9304
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2021-10-28 22:19:05 +0200

    upgrades people

--- plotly_playground2.py
+++ plotly_playground2.py
@@ -2,24 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.special
 
+
+def T(n):
+    if n == 1:
+        return 0
+    else:
+        return np.sqrt(1+(n - 1))
+
+
 # 100 linearly spaced angles between 0 and pi/3
-x = np.linspace(0, 40, 41)
+x = np.linspace(0, 10000000, 10000001)
 
 # def y
-# y = (np.sqrt(x+np.sqrt(x))-np.sqrt(x))
-#y = (np.power(-2, x)/(np.power(x, 2)+1))
-#y = ((1+np.power(-1, x))*((x+1)/(x))+np.power(-1, x))
-#y = (scipy.special.factorial(x)/np.power(x, x))
-#y = ((np.power(-1, x)*(x+(2/x)-np.sqrt(np.power(x, x)+4*x))))
-#y = (np.power(-2, x)/(np.power(x,x)+1))
-y = (np.power(-1, x)*(x+(x/2)-np.sqrt(1+(4/x))))
+y = [T(i) for i in range(1, 10000002)]
 
 # setting the axes at the centre
 fig = plt.figure()
-
 plt.plot(x, y, linewidth=0.5, color="black")
 plt.plot(x, y, 'ro')
 plt.tick_params(bottom=False, left=False)
-plt.title('(1+np.power(-1, x))*((x+1)/(x))+np.power(-1, x)')
-plt.savefig('ana_1_b.pdf')
+plt.title('plot A2')
+plt.savefig('a2.pdf')
 plt.show()
