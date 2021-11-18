d3b111bc5e2eeabb6cb3d16584f399e8d692dc31
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2021-11-18 19:45:47 +0100

    error calc and shit

--- Praktikum_GK/error_calculus.py
+++ Praktikum_GK/error_calculus.py
@@ -18,6 +18,24 @@ error = 0
 for i in values:
     error += (i-x)**2
 
-print(error)
 error = np.sqrt(error/42)
 print(error)
+
+
+T = 630
+tau = -0.04876
+r = 0.047
+d = 0.050
+S = 0.179
+M = 1.5
+L = 5.38
+G = 6.6742*(10**(-11))
+
+#temp = ((4*np.pi**2)/T**2)+(1/(tau**2))
+#G = (temp*(r**2)*d*S)/(4*M*L)
+
+temp = (4*G*M*L)/((r**2)*d*S)-(4*np.pi**2)/(T**2)
+print(temp)
+tau = 1/(np.sqrt(temp))
+print(G)
+print(tau)
--- Praktikum_GK/graph_2_error.py
+++ Praktikum_GK/graph_2_error.py
@@ -27,24 +27,7 @@ x = [300,
 2809.333333,
 ]
 y = np.log(ytemp)
-t1 = [y[0],0]
-t2 = [y[1], 0]
-t3 = [y[2], 0]
-t4 = [y[3], 0]
-t5 = [y[4], 0]
-t6 = [y[5], 0]
-t7 = [y[6], 0]
-t8 = [y[7], 0]
-t9 = [y[8], 0]
-x1 = [x[0],x[0]]
-x2 = [x[1],x[1]]
-x3 = [x[2],x[2]]
-x4 = [x[3],x[3]]
-x5 = [x[4],x[4]]
-x6 = [x[5],x[5]]
-x7 = [x[6],x[6]]
-x8 = [x[7],x[7]]
-x9 = [x[8],x[8]]
+
 
 plt.plot(x, y, 'ko', markersize=5, color="black")
 slope_intercept = np.polyfit(x, y, 1)
@@ -53,15 +36,6 @@ slope_intercept = slope_intercept.replace("  ", "x + ")
 slope_intercept = slope_intercept.replace("[", "")
 slope_intercept = slope_intercept.replace("]", "")
 plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)), linewidth='0.5', linestyle='dashed', color='black', label=r'$f(x) = $'+str(slope_intercept))
-plt.plot(x1,t1, 'k', linewidth=0.5, linestyle='-')
-plt.plot(x2,t2, 'k', linewidth=0.5, linestyle='-')
-plt.plot(x3,t3, 'k', linewidth=0.5, linestyle='-')
-plt.plot(x4,t4, 'k', linewidth=0.5, linestyle='-')
-plt.plot(x5,t5, 'k', linewidth=0.5, linestyle='-')
-plt.plot(x6,t6, 'k', linewidth=0.5, linestyle='-')
-plt.plot(x7,t7, 'k', linewidth=0.5, linestyle='-')
-plt.plot(x8,t8, 'k', linewidth=0.5, linestyle='-')
-plt.plot(x9,t9, 'k', linewidth=0.5, linestyle='-')
 plt.ylabel(r'$\ln(A_i)$')
 plt.xlabel(r'$t\:(min)$')
 plt.title(r'$Deflection\:as\:function\:of\:time.$')
@@ -69,3 +43,4 @@ plt.legend(loc='upper right')
 plt.grid(color='k', linestyle='--', linewidth=0.2)
 plt.savefig('graph_1_evaluation.pdf')
 plt.show()
+print(slope_intercept)
