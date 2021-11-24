8cdf78ec305200c41daa04ffe30f59b907988c9f
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2021-11-24 19:20:30 +0100

    praktikum results

--- Praktikum_ES/error_calculus_a.py
+++ Praktikum_ES/error_calculus_a.py
@@ -1,40 +1,35 @@
 import numpy as np
 
 """For calculating the medium value for x_0"""
-values = [318,
-          312.6,
-          314.4,
-          323.4,
-          302.4,
-          277.8,
-          343.8,
-          344.4,
-          286.2
-          ]
+values = [16.8,
+          16.6,
+          16.4,
+          16.4,
+          15.9, ]
 y = int(len(values))
 x = 0
 
 for i in values:
     x += i
 x = (x / y)
-print('The medium value for x_0 is: '+str(x))
+print('The medium value for x_0 is: ' + str(x))
 
 """For calculating the error upon x_0"""
 error = 0
 for i in values:
-    error += (i - x)**2
+    error += (i - x) ** 2
 error = np.sqrt((1 / (y * (y - 1))) * error)
 print('The error upon x_0 is+-: ' + str(error))
 
 """For calculating the expected value for x_0"""
-k = 0.5
-y_r = 2
-y_s = 5
-x_0 = np.sqrt((2/k)*y_r*(y_s-y_r))
-print('The expected value for x_0 is: '+str(x_0))
+k = 23/30
+y_s = 22.2
+y_r = 10.5
+x_0 = np.sqrt((2/k)*y_r*(y_s - y_r))
+print('The expected value for x_0 is: ' + str(x_0))
 
 """For calculating the velocity"""
 g = 9.81
-t = np.sqrt((2*y_r)/g)
-v_1 = x/t
-print('The value for v_1 should be: '+str(v_1))
+t = np.sqrt((2 * y_r) / g)
+v_1 = x / t
+print('The value for v_1 should be: ' + str(v_1))
--- Praktikum_ES/error_calculus_b.py
+++ Praktikum_ES/error_calculus_b.py
@@ -1,15 +1,11 @@
 import numpy as np
 
 """For calculating the medium value for x_0"""
-values = [318,
-          312.6,
-          314.4,
-          323.4,
-          302.4,
-          277.8,
-          343.8,
-          344.4,
-          286.2
+values = [18,
+          18.1,
+          18.1,
+          18.1,
+          18,
           ]
 y = int(len(values))
 x = 0
@@ -17,11 +13,11 @@ x = 0
 for i in values:
     x += i
 x = (x / y)
-print('The medium value for x_0 is: '+str(x))
+print('The medium value for x_0 is: ' + str(x))
 
 """For calculating the error upon x_0"""
 error = 0
 for i in values:
-    error += (i - x)**2
+    error += (i - x) ** 2
 error = np.sqrt((1 / (y * (y - 1))) * error)
 print('The error upon x_0 is+-: ' + str(error))
