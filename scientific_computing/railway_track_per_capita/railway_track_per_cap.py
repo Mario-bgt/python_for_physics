d3cb2ef139990a29d225f3b5b228324028c27819
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2022-03-18 17:54:01 +0100

    track scheiss

--- scientific_computing/railway_track_per_capita/railway_track_per_cap.py
+++ scientific_computing/railway_track_per_capita/railway_track_per_cap.py
@@ -1,17 +1,38 @@
 import csv
 
-filename = 'rail_lines.csv'
-file = open(filename, encoding='utf-8')
-table = csv.reader(file, delimiter=',', quotechar='"')
-table = list(table)
+filename1 = 'rail_lines.csv'
+file1 = open(filename1, encoding='utf-8')
+table1 = csv.reader(file1, delimiter=',', quotechar='"')
+table1 = list(table1)
 
 rail = {}
-for i, row in enumerate(table[4:20]):
-    for j, col in enumerate(row):
+for i, row in enumerate(table1[5:]):
+    for j, col in enumerate(row[50:51]):
         if col != '':
-            if i not in rail:
-                rail[i] = {}
-            rail[i][j] = col
+            if row[1] not in rail:
+                rail[row[1]] = {}
+            rail[row[1]][2006] = float(col)
 
-for r in rail:
-    print(r, rail[r])
+filename2 = 'population.csv'
+file2 = open(filename2, encoding='utf-8')
+table2 = csv.reader(file2, delimiter=',', quotechar='"')
+table2 = list(table2)
+
+population = {}
+for i, row in enumerate(table2[5:]):
+    for j, col in enumerate(row[4:]):
+        if col != '':
+            if row[1] not in population:
+                population[row[1]] = {}
+            population[row[1]][j + 1960] = float(col)
+
+maximum = float(0)
+maximum_i = ''
+for i in rail:
+    if rail[i][2006] != '' and population[i][2006] != '':
+        track_per_pop = (rail[i][2006])*1000 / (population[i][2006])
+        if track_per_pop > maximum:
+            maximum = track_per_pop
+            maximum_i = i
+
+print(str(maximum_i), str(maximum))
