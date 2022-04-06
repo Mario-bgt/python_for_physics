0d9c3814426f07cf2840c5b0578543f8a923efc4
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2022-04-06 23:30:54 +0200

    gungigungangu

--- py124/ex2.py
+++ py124/ex2.py
@@ -2,21 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 
+matplotlib.rcParams['text.usetex'] = True
+
+
 pi = np.pi
-Q = 1 * 10 ** (-16)
-q = 1.6 * 10 ** (-19)
-R = 0.001
-e_0 = 8.85418782 * 10 ** (-12)
-p = 1.8*10**(-6)
+E_m = 1
+v_0 = 0.5
+R = 1
 
-x = np.linspace(0, 1, 100)
-y = (p*x)/(3*e_0)
+x = np.linspace(0, R, 10000)
+y = (E_m*x)/R
+z = (v_0/2)*(3-(x**2)/(R**2))
+x2 = np.linspace(R, R*5, 10000)
+y2 = (E_m*R**2)/(x2**2)
+z2 = (v_0*R)/x2
 
-x2 = np.linspace(1, 2, 100)
-y2 = (p*R**3)/(3*e_0*x**2)
-
-plt.plot(x, y, 'k', linewidth=0.5, linestyle='--')
-plt.plot(x2, y2, 'k', linewidth=0.5, linestyle='--')
+fig = plt.figure()
+plt.plot(x, y, 'r', linewidth=0.5, linestyle='--', label=r'$E(r)$')
+plt.plot(x2, y2, 'r', linewidth=0.5, linestyle='--')
+plt.plot(x, z, 'g', linewidth=0.5, linestyle='--', label=r'$V(r)$')
+plt.plot(x2, z2, 'g', linewidth=0.5, linestyle='--')
 plt.grid(color='k', linestyle='--', linewidth=0.2)
+plt.ylabel(r'$E(r)\:V(r)$')
+plt.xlabel(r'$Distance\:from\:center$')
+plt.title(r'$Physics\:homework\:exercise\:2$')
+plt.legend(loc='best')
 plt.savefig('graph_2.pdf')
 plt.show()
