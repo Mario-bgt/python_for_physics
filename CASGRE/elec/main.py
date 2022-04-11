125db47489897c6c5eebd714dd6e87e60697d676
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2022-04-12 00:54:58 +0200

    smol clean up

--- CASGRE/elec/main.py
+++ CASGRE/elec/main.py
@@ -5,9 +5,9 @@ from dash import html
 import plotly.express as px
 import simplekml
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']  # Design
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-dfp = pd.read_pickle('a_file.pkl')  # Startexcel einlesen
+dfp = pd.read_pickle('a_file.pkl')
 
 fig = px.scatter_mapbox(dfp, lat="lat", lon="lon",
                         hover_name="xtf_id",
@@ -17,18 +17,18 @@ fig = px.scatter_mapbox(dfp, lat="lat", lon="lon",
                         height=800)
 fig.update_layout(mapbox_style="open-street-map")
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)  # CSS Style
-app.layout = html.Div([  # app Layout
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div([
     html.H1('Elektrizitätsproduktionsanlagen der Schweiz'),
-    html.H6('Auswahl der Hauptkategorie'),  # Titel
-    dcc.Checklist(  # dropdown Menue mit allen Wagen
+    html.H6('Auswahl der Hauptkategorie'),
+    dcc.Checklist(
         id='main_cat',
         options=['Wasserkraft', 'Übrige erneuerbare Energien', 'Kernenergie', 'Fossile Energieträger'],
         value=['Übrige erneuerbare Energien'],
         inline=True
     ),
-    html.H6('Auswahl der Produktionskategorie'),  # Titel
-    dcc.Checklist(  # dropdown Menue mit allen Wagen
+    html.H6('Auswahl der Produktionskategorie'),
+    dcc.Checklist(
         id='plant_cat',
         options=['Abwasserkraftwerk', 'Ausleitkraftwerk', 'Dotierwasserkraftwerk', 'Durchlaufkraftwerk',
                  'Trinkwasserkraftwerk', 'Pumpspeicherkraftwerk', 'Speicherkraftwerk', 'Angebaut', 'Integriert',
@@ -38,8 +38,8 @@ app.layout = html.Div([  # app Layout
                'Freistehend', 'Biomassenutzung', 'Kehrichtverbrennung', 'Abwasserreinigung'],
         inline=True
     ),
-    html.H6('Auswahl der Unterkategorie'),  # Titel
-    dcc.Checklist(  # dropdown Menue mit allen Wagen
+    html.H6('Auswahl der Unterkategorie'),
+    dcc.Checklist(
         id='sub_cat',
         options=['Wasserkraft', 'Photovoltaik', 'Windenergie', 'Biomasse', 'Geothermie',
                  'Kernenergie', 'Erdöl', 'Erdgas', 'Kohle', 'Abfälle'],
@@ -50,13 +50,13 @@ app.layout = html.Div([  # app Layout
     dcc.Input(id="minimum", type="number", placeholder=0, value=0),
     dcc.Input(id="maximum", type="number", placeholder=1872000, value=1872000),
     html.Div(
-        [html.Button("Download as HTML", id="btn_html"),  # Knopf für HTML export
-         dcc.Download(id="download")]  # id für HTML export
+        [html.Button("Download as HTML", id="btn_html"),
+         dcc.Download(id="download")]
     ),
-    dcc.Graph(  # Bild des Graph mit analyse Option
+    dcc.Graph(
         id='map',
         figure=fig,
-        style={  # Style der Map
+        style={
             'width': '100%', 'height': '800px', 'lineHeight': '0px',
             'borderWidth': '0px', 'borderStyle': 'dashed',
             'borderRadius': '0px', 'textAlign': 'center', 'margin': '1px'
@@ -65,14 +65,14 @@ app.layout = html.Div([  # app Layout
 
 
 @app.callback(
-    dash.dependencies.Output('map', 'figure'),  # Die geplottete figur als Output definieren
+    dash.dependencies.Output('map', 'figure'),
     [dash.dependencies.Input('main_cat', 'value'),
      dash.dependencies.Input('plant_cat', 'value'),
      dash.dependencies.Input('sub_cat', 'value'),
      dash.dependencies.Input('minimum', 'value'),
      dash.dependencies.Input('maximum', 'value'),
      ])
-def update_output(main_cat, plant_cat, sub_cat, minimum, maximum):  # erstellen des Plot und Rückgabe plot
+def update_output(main_cat, plant_cat, sub_cat, minimum, maximum):
     global fig
     df = dfp
     df = df[df.Total_Power > minimum]
@@ -94,17 +94,18 @@ def update_output(main_cat, plant_cat, sub_cat, minimum, maximum):  # erstellen 
     kml.save('currentplot.kml')
     return fig
 
-@app.callback(  # Ausgabe der HTML Version
-    dash.dependencies.Output('download', 'data'),  # Output definieren
-    [dash.dependencies.Input('btn_html', 'n_clicks')]  # Button HTMl als input definieren
+@app.callback(
+    dash.dependencies.Output('download', 'data'),
+    [dash.dependencies.Input('btn_html', 'n_clicks')]
 )
-def download(n_clicks):  # Ausgabe der HTML Version
-    if n_clicks is None:  # Falls der button nicht gedrückt wurde
+def download(n_clicks):
+    if n_clicks is None:
         print('Button HTML has not been pressed yet')
-    else:  # Falls der Button gedrückt wurde
+    else:
         return dcc.send_file(
-            "currentmap.html"  # Ausgabe der HTML Datei
+            "currentmap.html"
         )
+
 
 if __name__ == '__main__':
     app.run_server(debug=False)
