f67d91c2b1544eb32b57ec0ff2903f06111da2bb
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2022-05-05 18:23:33 +0200

    puzle nün

--- scientific_computing/puzzle/puzzle_9/puzzle_9.py
+++ scientific_computing/puzzle/puzzle_9/puzzle_9.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-g = 10  # gravitational acceleration
+g = 9.81
 L = 0.03  # radius of the wheel
 l = 0.4  # length of pendulum
 ohm = 10  # angular speed of the wheel
@@ -10,7 +10,7 @@ omega = np.sqrt(g / l)  # natural frequency of the pendulum
 tau = [0]  # location of the wheel
 lama = L/l
 gamma = (omega**2)/(ohm**2)
-g = 9.81
+
 
 v_phi = [0]
 v_tau=[0]
