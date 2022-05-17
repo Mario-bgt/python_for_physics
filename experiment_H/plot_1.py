d924ce18b6bc5e180a1a49f3412e21d5d16f6480
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2022-05-17 18:14:23 +0200

    exp h

--- experiment_H/plot_1.py
+++ experiment_H/plot_1.py
@@ -6,9 +6,16 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure()
 x = [8, 7, 6, 5, 4, 3, 2, 1]
-y = [11.86, 11.23, 10.33, 9.61, 8.3, 6.39, 4.44, 2.21]
+ytemp = [11.86, 11.23, 10.33, 9.61, 8.3, 6.39, 4.44, 2.21]
 xerr = [0.16, 0.14, 0.12, 0.1, 0.08, 0.06, 0.04, 0.02]
-yerr = [0.33, 0.31, 0.28, 0.27, 0.23, 0.18, 0.12, 0.06]
+yerrtemp = [0.33, 0.31, 0.28, 0.27, 0.23, 0.18, 0.12, 0.06]
+y = []
+yerr = []
+for i in ytemp:
+    y.append(i*0.1)
+for j in yerrtemp:
+    yerr.append(j*0.1)
+
 
 plt.errorbar(x=x, y=y, xerr=xerr, yerr=yerr, fmt='.k', elinewidth=0.5, markersize=2.5, capsize=1, color="black",
              label=r'$values$')
--- experiment_H/plot_2.py
+++ experiment_H/plot_2.py
@@ -6,9 +6,15 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure()
 x = [8, 7, 6, 5, 4, 3, 2, 1]
-y = [11.66, 10.93, 9.62, 9.44, 8.09, 6.28, 4.27, 2.3]
+ytemp = [11.66, 10.93, 9.62, 9.44, 8.09, 6.28, 4.27, 2.3]
 xerr = [0.16, 0.14, 0.12, 0.1, 0.08, 0.06, 0.04, 0.02]
-yerr = [0.24, 0.23, 0.2, 0.2, 0.17, 0.13, 0.09, 0.05]
+yerrtemp = [0.24, 0.23, 0.2, 0.2, 0.17, 0.13, 0.09, 0.05]
+y = []
+yerr = []
+for i in ytemp:
+    y.append(i*0.1)
+for j in yerrtemp:
+    yerr.append(j*0.1)
 
 plt.errorbar(x=x, y=y, xerr=xerr, yerr=yerr, fmt='.k', elinewidth=0.5, markersize=2.5, capsize=1, color="black",
              label=r'$values$')
