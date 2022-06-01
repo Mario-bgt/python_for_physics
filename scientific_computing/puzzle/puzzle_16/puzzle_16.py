ba3b8658b864e9b6686be75b8895652d3cd56907
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2022-06-01 19:49:34 +0200

    gungi gung gangu

--- scientific_computing/puzzle/puzzle_16/puzzle_16.py
+++ scientific_computing/puzzle/puzzle_16/puzzle_16.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy.fft as npf
 
+
 def V(x):
     return (1/2)*x**2
 
@@ -19,28 +20,24 @@ def psi_n(x, n):
 
 
 N = 2**9
-n = np.arange(N)-N//2
 dt = 0.001
 dx = (2*np.pi/N)**.5
-t_max = 300
 x = np.linspace(-np.pi, np.pi, N//2)
-t = 0
 dk = (2 * np.pi) / (N * dx)
 k = np.arange(N - N // 2) * dk
-psy = psi_n(x, 2)
+function = psi_n(x, 3)
 while True:
-    fft = np.fft.fftshift(np.fft.fft(np.fft.fftshift(psy)))
+    fft = np.fft.fftshift(np.fft.fft(np.fft.fftshift(function)))
     fft = fft*np.exp(-1j*k**2*(dt/2))
     ift = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(fft)))
-    for i, x_n in enumerate(x):
-        ift[i] = ift[i]*np.exp(-1j*V(x_n)*dt)
+    ift = ift*np.exp(-1j*V(x)*dt)
     fft = np.fft.fftshift(np.fft.fft(np.fft.fftshift(ift)))
     fft = fft*np.exp(-1j*k**2*(dt/2))
-    psy = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(fft)))
+    function = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(fft)))
     real = []
     imag = []
     prob = []
-    for i in psy:
+    for i in function:
         real.append(i.real)
         imag.append(i.imag)
         prob.append(np.abs(i)**2)
@@ -50,6 +47,3 @@ while True:
     plt.plot(imag)
     plt.pause(0.1)
     plt.gca().clear()
-    t += dt
-
-
