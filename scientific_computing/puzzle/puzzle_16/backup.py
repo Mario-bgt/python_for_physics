6ad488cbb2d0623a94bf81c32fbdc69f6d26dde8
Mario-bgt <87278419+Mario-bgt@users.noreply.github.com>
2022-06-02 22:43:20 +0200

    fix dies fix das

--- scientific_computing/puzzle/puzzle_16/backup.py
+++ scientific_computing/puzzle/puzzle_16/backup.py
@@ -1,56 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.fft as npf
+
+plt.style.use('dark_background')
 
 
 def V(x):
-    return (1/2)*x**2
+    return (1 / 2) * x ** 2
 
 
 def psi_0(x):
-    return np.pi**(-1/4)*np.exp(-x**2/2)
+    return np.pi ** (-1 / 4) * np.exp(-x ** 2 / 2)
 
 
 def psi_n(x, n):
     if n == 1:
-        return np.sqrt(2/n)*x*psi_0(x)
-    return np.sqrt(2/n)*x*psi_n(x, n-1)-np.sqrt((n-1)/n)*psi_n(x, n-2)
+        return psi_0(x)
+    if n == 2:
+        return np.sqrt(2 / n) * x * psi_0(x)
+    return np.sqrt(2 / n) * x * psi_n(x, n - 1) - np.sqrt((n - 1) / n) * psi_n(x, n - 2)
 
 
-N = 2**8
-n = np.arange(N)-N/2
-dt = 0.01
-dx = (2*np.pi/N)**.5
-t_max = 300
-x = np.arange((N)-N//2) * dx
-t = 0
-k = (2 * np.pi) / (N * dx) * n
-psy = psi_0(x)
-#plt.plot(psy)
-#plt.show()
-#np.fft.fftshift(np.fft(np.fftshift()))
-while t < t_max:
-    fft = np.fft.fft(psy)
-    fft = fft*np.exp(-1j*k**2*(dt/2))
-    ift = np.fft.ifft(fft)
-    for i, x_n in enumerate(x):
-        ift[i] = ift[i]*np.exp(-1j*V(x_n)*dt)
-    fft = np.fft.fft(ift)
-    fft = fft*np.exp(-1j*k**2*(dt/2))
-    psy = np.fft.ifft(fft)
+N = 2 ** 9
+dt = 0.001
+dx = (2 * np.pi / N) ** .5
+x = np.linspace(-np.pi, np.pi, N//2)
+dk = (2 * np.pi) / (N * dx)
+k = np.arange(N - N // 2) * dk
+function = psi_n(x, 2)
+plt.plot(function)
+plt.show()
+while True:
+    fft = np.fft.fftshift(np.fft.fft(np.fft.fftshift(function)))
+    fft = fft * np.exp(-1j * k ** 2 * (dt / 2))
+    ift = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(fft)))
+    ift = ift * np.exp(-1j * V(x) * dt)
+    fft = np.fft.fftshift(np.fft.fft(np.fft.fftshift(ift)))
+    fft = fft * np.exp(-1j * k ** 2 * (dt / 2))
+    function = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(fft)))
     real = []
     imag = []
     prob = []
-    for i in psy:
-        print(i, i.real, i.imag)
+    for i in function:
         real.append(i.real)
         imag.append(i.imag)
-        prob.append(np.abs(i)**2)
-    plt.ylim(0,1)
+        prob.append(np.abs(i) ** 2)
+    plt.ylim(-1, 1)
     plt.plot(prob)
-    #plt.plot(real)
-    #plt.plot(imag)
-    plt.pause(0.1)
+    plt.plot(real)
+    plt.plot(imag)
+    plt.pause(0.01)
     plt.gca().clear()
-    t += dt
-
-
